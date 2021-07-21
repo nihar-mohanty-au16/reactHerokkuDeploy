293c07661fa3fc0cf70522b53cbe36ec8fad7814
nihar-mohanty-au16 <mnihar198@gmail.com>
2021-07-21 22:27:30 +0530

    clock

---
+++ clock/my-app/src/component/Clock.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+import './Clock.css'
+
+const Clock = () => {
+    let time = new Date().toLocaleTimeString();
+    const [Ctime, setCtime] = useState(time)
+    const updateTime = () => {
+        time = new Date().toLocaleTimeString();
+        setCtime(time)
+    }
+
+    setInterval(updateTime,1000)
+    return (
+        <div className = "time">
+            <h1>{time}</h1>
+        </div>
+    )
+}
+
+export default Clock
